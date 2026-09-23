9b3db3d4bee970f80e49d21894ff6cb06a46ace1
Josebahuegun <jhuegun25@izarraitz.eus>
2026-09-23 09:42:48 +0200

    Textua eta botoia sortu

--- src/main/java/eus/adibidea/Main.java
+++ src/main/java/eus/adibidea/Main.java
@@ -1,14 +1,27 @@
 package eus.adibidea;
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage stage){
-        StackPane root = new StackPane();
+        Pane root = new Pane();
+
+        Label label = new Label("Kaixoooo");
+        label.setLayoutX(100);
+        label.setLayoutY(100);
+
+        Button button = new Button("Ados");
+        label.setLayoutX(300);
+        label.setLayoutY(300);
+
+        root.getChildren().addAll(label, button);
+
         Scene scene = new Scene(root);
 
         stage.setTitle("JavaFX aplikazioa");
