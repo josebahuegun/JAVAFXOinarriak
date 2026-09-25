6d44e4c578ab85cadf62e56961f76d69f29f99d7
Josebahuegun <jhuegun25@izarraitz.eus>
2026-09-25 08:06:30 +0200

    vbox layouta

--- src/main/java/eus/adibidea/Main.java
+++ src/main/java/eus/adibidea/Main.java
@@ -3,24 +3,24 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage stage){
-        Pane root = new Pane();
+        VBox root = new VBox();
 
         Label label = new Label("Kaixoooo");
-        label.setLayoutX(100);
-        label.setLayoutY(100);
 
-        Button button = new Button("Ados");
-        label.setLayoutX(300);
-        label.setLayoutY(300);
+        Button adosBttn = new Button("Ados");
+        Button utziBttn = new Button("Ados");
 
-        root.getChildren().addAll(label, button);
+
+        root.getChildren().addAll(label, adosBttn, utziBttn);
 
         Scene scene = new Scene(root);
 
